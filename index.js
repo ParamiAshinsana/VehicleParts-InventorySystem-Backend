f7f281667e71d2824d690192e7805fc97a3e5de5
Parami <paramiashinsana@gmail.com>
2025-06-18 09:44:11 +0530

    added main routes in index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json()); 
+
+
+const batteryRoutes = require('./routes/batteryRoutes');
+const tireRoutes = require('./routes/tireRoutes');
+const oilFilterRoutes = require('./routes/oilFilterRoutes');
+
+app.use('/batteries', batteryRoutes);
+app.use('/tires', tireRoutes);
+app.use('/oilfilters', oilFilterRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
